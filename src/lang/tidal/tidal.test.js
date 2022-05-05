0ca12187d0536894472b4b6c32fec752dfcafbcb
ghalestrilo <ghalestrilo@gmail.com>
2022-05-05 13:28:57 -0300

    :sparkles: create yarn gen script

--- src/lang/tidal/tidal.test.js
+++ src/lang/tidal/tidal.test.js
@@ -1,8 +1,8 @@
-const peggy = require('peggy')
+import { parse } from './parser'
 
 describe('tidal language support', () => {
   it('parses file correctly', () => {
     expect(0).toEqual(0)
-    
+    parse('./song1.hs')
   })
 })
