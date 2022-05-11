d56f82832fdec27e2d68ffda77ef9d1961c221ce
ghalestrilo <ghalestrilo@gmail.com>
2022-05-11 00:14:22 -0300

    :construction: stub layout

--- src/pages/Session.jsx
+++ src/pages/Session.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Container, Grid, Header } from "semantic-ui-react";
+import {
+  Container,
+  Divider,
+  Form,
+  Grid,
+  Header,
+  TextArea,
+} from "semantic-ui-react";
 
 const instruments = ["drums", "keys", "blips"];
-const scenes = ["asd1", "asd2", "asd3"].map((name) => ({
+const scenes = ["Scene 1", "Scene 2", "Scene 3"].map((name) => ({
   name,
   actions: Object.fromEntries(
     ["blips", "drums", "keys"].map((instrumentName) => [
@@ -13,26 +20,61 @@ const scenes = ["asd1", "asd2", "asd3"].map((name) => ({
 }));
 const trackname = "Trackname";
 
+const SceneGrid = () => (
+  <Grid columns={instruments.length + 1} divided>
+    <Grid.Row>
+      <Grid.Column>
+        <Header as="h4">Scene</Header>
+      </Grid.Column>
+      {instruments.map((instrumentName) => (
+        <Grid.Column>
+          <Header as="h4">{instrumentName}</Header>
+        </Grid.Column>
+      ))}
+    </Grid.Row>
+
+    {scenes.map(({ name, actions }) => (
+      <Grid.Row>
+        <Grid.Column>{name}</Grid.Column>
+        {instruments.map((instrumentName) => (
+          <Grid.Column>{actions[instrumentName]}</Grid.Column>
+        ))}
+      </Grid.Row>
+    ))}
+  </Grid>
+);
+
+const Console = () => {
+  const contents = "lorem ipsum";
+
+  return <Container>{contents}</Container>;
+};
+
+const Editor = () => {
+  return (
+    <Form>
+      <Form.TextArea disabled />
+    </Form>
+  );
+};
+
 const SessionPage = () => {
   return (
-    <Container>
+    <Container fluid style={{ padding: "2rem" }}>
       <Header as={"h1"}>{trackname}</Header>
-      <Grid columns={instruments.length + 1} divided>
-        <Grid.Row>
-          <Grid.Column>Scene</Grid.Column>
-          {instruments.map((instrumentName) => (
-            <Grid.Column>{instrumentName}</Grid.Column>
-          ))}
-        </Grid.Row>
-
-        {scenes.map(({ name, actions }) => (
+      <Grid columns={2} divided>
+        <Grid.Column>
           <Grid.Row>
-            <Grid.Column>{name}</Grid.Column>
-            {instruments.map((instrumentName) => (
-              <Grid.Column>{actions[instrumentName]}</Grid.Column>
-            ))}
+            <SceneGrid />
           </Grid.Row>
-        ))}
+          <Divider />
+          <Grid.Row>
+            <Console />
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column>
+          <Editor />
+        </Grid.Column>
       </Grid>
     </Container>
   );
