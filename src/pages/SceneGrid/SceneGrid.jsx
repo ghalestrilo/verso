1a92c50984fbed2c061d9176d3f71f1b606ae5a5
ghalestrilo <ghalestrilo@gmail.com>
2022-05-21 18:25:43 -0300

    display actions as a Table

--- src/pages/SceneGrid/SceneGrid.jsx
+++ src/pages/SceneGrid/SceneGrid.jsx
@@ -1,34 +1,42 @@
 import React from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
 
 const SceneGrid = ({ track }) => {
   // console.log(track);
   console.log(track);
-  return (
-    <Grid columns={(track?.channels?.length || 0) + 1} divided>
-      <Grid.Row>
-        <Grid.Column>
-          <Header as="h4">Scene</Header>
-        </Grid.Column>
-        {track?.channels?.map((channelName) => (
-          <Grid.Column>
-            <Header as="h4">{channelName}</Header>
-          </Grid.Column>
-        ))}
-      </Grid.Row>
+  const { channels, scenes } = track;
 
-      {track?.scenes?.map(
-        ({ name, actions }) =>
-          actions && (
-            <Grid.Row>
-              <Grid.Column>{name}</Grid.Column>
-              {track?.channels?.map((channelName) => (
-                <Grid.Column>{actions[channelName]}</Grid.Column>
-              ))}
-            </Grid.Row>
-          )
-      )}
-    </Grid>
+  const fireScene = () => alert("haha");
+
+  return (
+    <>
+      <Table celled selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Scene</Table.HeaderCell>
+            {channels.map((channelName) => (
+              <Table.HeaderCell>{channelName}</Table.HeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {scenes.map(
+            ({ name, actions }) =>
+              actions && (
+                <Table.Row>
+                  <Table.Cell onClick={fireScene}>{name}</Table.Cell>
+                  {channels.map((channelName) => (
+                    <Table.Cell onClick={fireScene}>
+                      {actions[channelName]}
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              )
+          )}
+        </Table.Body>
+      </Table>
+    </>
   );
 };
 
