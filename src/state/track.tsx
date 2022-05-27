31184be065e3bc34b809484ce7b4e698645daf65
ghalestrilo <ghalestrilo@gmail.com>
2022-05-26 23:49:51 -0300

    store filename

--- src/state/track.tsx
+++ src/state/track.tsx
@@ -20,24 +20,21 @@ const parseTrack = (data: any) => (state: State) => ({
 });
 
 export const useTrackState = create<State>((set) => ({
-  name: "Testname",
-  raw: undefined,
+  name: "Filename",
+  raw: "",
   // TODO: remove mock data
-  channels: ["drums", "keys", "blips"],
-  scenes: ["Scene 1", "Scene 2", "Scene 3"].map((name) => ({
-    name,
-    actions: Object.fromEntries(
-      ["blips", "drums", "keys"].map((instrumentName) => [
-        instrumentName.toString(),
-        `${instrumentName} pattern`,
-      ])
-    ),
-  })),
+  channels: [],
+  scenes: [],
   setTrackData: (data: string) => {
     // TODO: destructure parsed data here, catch errors before merging objects
     return set(parseTrack(data));
   },
   loadFile: async (filename: string) => {
-    loadFile(filename).then(({ data }) => set(parseTrack(data)));
+    loadFile(filename).then(({ data }) =>
+      set((state) => ({
+        ...parseTrack(data)(state),
+        filename,
+      }))
+    );
   },
 }));
