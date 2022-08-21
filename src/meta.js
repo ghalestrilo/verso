6644b14fee5749db1ffa13781df3505ba76f6286
ghalestrilo <ghalestrilo@gmail.com>
2022-08-20 22:03:31 -0300

    fix intro file

--- src/meta.js
+++ src/meta.js
@@ -13,34 +13,33 @@ export const examples = {
   },
   fileData: {
     tidal: `-- Triangles
-    do
-      -- @name triangles
-      setcps 2
-      d1
-        $ (n . fastcat)
-          [ randcat ["13", "13", "2"]
-          , "10"
-          , randcat ["13", "5", "11", "2", "6"]
-          ]
-        # pan (range (-1) 1 $ rand)
-        # s "gretsch"
-        # bus
-        # lpf 8000
-        # fader 1
-      d2 $ (note . root . scale "minor") "<2 4 6>@2 <0 2 3>"
-        # s "supervibe"
-      d3 $ (note . root . scale "minor") ("{0@5 1@4 0@8 1}%3" |- 4 |+ "[-3,0,2,6]")
-        # s "superpiano"
-      d4 $ (note . root . scale "minor") "{4 5 7 11@4 10@5 9 7 5 8 6 4@4}%3"
-          # s "superpiano"
-          # sus 2
-          # lpf 800
-    
-    do
-      -- @name add a third lol
-      d4 $ (note . root . scale "minor") ("{4 5 7 11@4 10@5 9 7 5 8 6 4@4}%3" |+ "[0,2]")
-          # s "superpiano"
-          # sus 2
-          # lpf 800`
+do
+  -- @name triangles
+  setcps 2
+  d1
+    $ (n . fastcat)
+      [ randcat ["13", "13", "2"]
+      , "10"
+      , randcat ["13", "5", "11", "2", "6"]
+      ]
+    # pan (range (-1) 1 $ rand)
+    # s "gretsch"
+    # lpf 8000
+  d2 $ (note . scale "minor") "<2 4 6>@2 <0 2 3>"
+    # s "supervibe"
+  d3 $ (note . scale "minor") ("{0@5 1@4 0@8 1}%3" |- 4 |+ "[-3,0,2,6]")
+    # s "virus2"
+  d4 $ (note . scale "minor") "{4 5 7 11@4 10@5 9 7 5 8 6 4@4}%3"
+      # s "virus1"
+      # sus 2
+      # lpf 800
+
+do
+  -- @name add a third lol
+  d4 $ (note . scale "minor") ("{4 5 7 11@4 10@5 9 7 5 8 6 4@4}%3" |+ "[0,2]")
+      # s "superpiano"
+      # sus 2
+      # lpf 800
+`
   }
 }
