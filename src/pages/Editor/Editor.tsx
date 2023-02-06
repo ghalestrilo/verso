6c99288b0c7b087dd4d290edfe70ac42ccad13d4
Thales <ghalestrilo@gmail.com>
2023-02-05 22:48:48 -0300

    debounce editor changes

--- src/pages/Editor/Editor.tsx
+++ src/pages/Editor/Editor.tsx
@@ -4,6 +4,7 @@ import { useTrackState } from "../../state/track";
 import CodeMirror from "@uiw/react-codemirror";
 import { StreamLanguage } from "@codemirror/language";
 import { haskell } from "@codemirror/legacy-modes/mode/haskell";
+import { debounce } from "lodash";
 
 const Editor = () => {
   const { rawLoaded, setTrackData } = useTrackState();
@@ -13,9 +14,7 @@ const Editor = () => {
       value={rawLoaded}
       height="700px"
       extensions={[StreamLanguage.define(haskell)]}
-      onChange={(value, viewUpdate) => {
-        setTrackData(value);
-      }}
+      onChange={debounce((value, viewUpdate) => setTrackData(value), 200)}
     />
   );
 };
